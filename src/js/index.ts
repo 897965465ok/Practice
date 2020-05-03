73f52331ac950ce363a6573b431c73c9e62b000a
Alfonso <897965465@qq.com>
2020-05-03 20:20:29 +0800

    BFC笔记

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,15 +1,13 @@
-
-
 import '../css/main.scss'
 // base64转 Blob
-// function dataURLtoBlob(dataurl) {
-//     var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-//       bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-//     while (n--) {
-//       u8arr[n] = bstr.charCodeAt(n);
-//     }
-//     return new Blob([u8arr], { type: mime });
-//   }
+function dataURLtoBlob(dataurl) {
+    let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+}
 
 // let button = document.querySelector('button')
 // button.onclick = () => {
